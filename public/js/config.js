80cec6d6ca4b5e85bca5f72496c6b9855138174e
Alexander Ose <alex@alexose.com>
2014-07-27 12:04:40 -0400

    adding yeoman-generated MEAN stack

---
+++ public/js/config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//Setting up route
+angular.module('mean').config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    // For unmatched routes:
+    $urlRouterProvider.otherwise('/');
+
+    // states for my app
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/index.html'
+    });
+}
+]);
+
+//Setting HTML5 Location Mode
+angular.module('mean').config(['$locationProvider',
+  function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}
+]);
